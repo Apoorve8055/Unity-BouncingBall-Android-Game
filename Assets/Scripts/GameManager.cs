4fc6a99628da04df60af21920b1abf798b029167
Apoorv <vapoorve@gmail.com>
2020-09-26 19:02:53 +0530

    BouncingBall Game Ready

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    int score;
+    public Text scroeText;
+    public GameObject gameStartUI;
+    private void Awake(){
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GameStart(){
+        gameStartUI.SetActive(false);
+        scroeText.gameObject.SetActive(true);
+    }
+    public void Restart(){
+        SceneManager.LoadScene("SampleScene");
+
+    }
+    public void ScoreUp(){
+        score++;
+        scroeText.text = score.ToString();
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed;
+
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void FixedUpdate(){
+        TouchMove();
+    }
+
+    void TouchMove(){
+        if(Input.GetMouseButton(0)){
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(touchPos.x <0 ){
+                //Move left
+                rb.velocity = Vector2.left * moveSpeed;
+            }else{
+                //Move right
+                rb.velocity = Vector2.right * moveSpeed;
+            }
+        }else{
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+
+
+}
